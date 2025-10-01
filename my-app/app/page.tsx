1d8061f2a2496d8e6ae24422dfe7485fec2dfb06
Nur Mohammod <nurm98371@gmail.com>
2025-10-02 00:19:18 +0600

    add

---
+++ my-app/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+     <h1 className="text-3xl text-violet-700 font-black items-center justify-center flex ">Welcome to Next.Js</h1>
+    </div>
+  );
+}
